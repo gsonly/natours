a2668c7492251ab5dbc6cc40810bd59db241bfa4
gsonly <gsonly.imo@gmail.com>
2021-01-09 00:34:10 +0300

    chore: serve static files with expres

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
 const { tourRoutes, userRoutes } = require('./routes')
+
+const assetspath = path.join(__dirname, '/public')
 
 const app = express()
 
 app.use(morgan('dev'))
 
 app.use(express.json())
+
+app.use(express.static(assetspath))
 
 app.use('/api/v1/tours', tourRoutes)
 
