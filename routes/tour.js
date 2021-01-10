df007d5be68adb2753123c177abf9f1a11a1efba
gsonly <gsonly.imo@gmail.com>
2021-01-10 15:07:37 +0300

    refactor: organize code

--- routes/tour.js
+++ routes/tour.js
@@ -1,11 +1,15 @@
 const { Router } = require('express')
-const { getTours, getTour, createTour, updateTour, deleteTour, checkID, checkBody } = require('../controllers')
+const {
+  getTours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+} = require('../controllers')
 
 const router = Router()
 
-router.param('id', checkID)
-
-router.route('/').get(getTours).post(checkBody, createTour)
+router.route('/').get(getTours).post(createTour)
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
--- server.js
+++ server.js
@@ -6,32 +6,6 @@ const { APP_PORT, MONGO_URI, MONGO_OPTIONS } = require('./config')
   try {
     await mongoose.connect(MONGO_URI, MONGO_OPTIONS)
 
-    const tourSchema = new mongoose.Schema({
-      name: {
-        type: String,
-        required: [true, 'a tour must have a name'],
-        unique: true,
-      },
-      rating: {
-        type: Number,
-        default: 4.5,
-      },
-      price: {
-        type: Number,
-        required: [true, 'a tour must have a price'],
-      },
-    })
-
-    const Tour = mongoose.model('Tour', tourSchema)
-
-    const testTour = new Tour({
-      name: 'the forest hiker',
-      price: 997,
-      rating: 4.7,
-    })
-
-    await testTour.save()
-
     app.listen(APP_PORT, console.log(`http://localhost:${APP_PORT}`))
   } catch (err) {
     console.log(err)
