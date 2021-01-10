b85cabdc358e895914b72983d0a6cd0ee50c7cdb
gsonly <gsonly.imo@gmail.com>
2021-01-10 11:06:12 +0300

    chore: setup mongoose & mongodb

--- config.js
+++ config.js
@@ -1,9 +1,28 @@
-const { NODE_ENV = 'development', APP_PORT = 3000 } = process.env
+const {
+  NODE_ENV = 'development',
+  APP_PORT = 3000,
+  MONGO_USERNAME = 'admin',
+  MONGO_PASSWORD = 'secret',
+  MONGO_HOST = 'localhost',
+  MONGO_PORT = 27017,
+  MONGO_DATABASE = 'natours',
+} = process.env
 
 const IN_PROD = NODE_ENV === 'production'
+
+const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`
+
+const MONGO_OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}
 
 module.exports = {
   NODE_ENV,
   APP_PORT,
   IN_PROD,
+  MONGO_URI,
+  MONGO_OPTIONS,
 }
--- server.js
+++ server.js
@@ -1,4 +1,39 @@
+const mongoose = require('mongoose')
 const app = require('./app')
-const { APP_PORT } = require('./config')
+const { APP_PORT, MONGO_URI, MONGO_OPTIONS } = require('./config')
 
-app.listen(APP_PORT, console.log(`http://localhost:${APP_PORT}`))
+;(async () => {
+  try {
+    await mongoose.connect(MONGO_URI, MONGO_OPTIONS)
+
+    const tourSchema = new mongoose.Schema({
+      name: {
+        type: String,
+        required: [true, 'a tour must have a name'],
+        unique: true,
+      },
+      rating: {
+        type: Number,
+        default: 4.5,
+      },
+      price: {
+        type: Number,
+        required: [true, 'a tour must have a price'],
+      },
+    })
+
+    const Tour = mongoose.model('Tour', tourSchema)
+
+    const testTour = new Tour({
+      name: 'the forest hiker',
+      price: 997,
+      rating: 4.7,
+    })
+
+    await testTour.save()
+
+    app.listen(APP_PORT, console.log(`http://localhost:${APP_PORT}`))
+  } catch (err) {
+    console.log(err)
+  }
+})()
