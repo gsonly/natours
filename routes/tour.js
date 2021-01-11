285098c1277f17a3e8eb0c9ee6df2cbf7634fb3d
gsonly <gsonly.imo@gmail.com>
2021-01-11 23:26:52 +0300

    feat: add tours staticsts

--- routes/tour.js
+++ routes/tour.js
@@ -6,6 +6,8 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../controllers')
 
 const router = Router()
@@ -13,6 +15,10 @@ const router = Router()
 router.route('/').get(getTours).post(createTour)
 
 router.route('/top-5-cheap').get(aliasTopTours, getTours)
+
+router.route('/tour-stats').get(getTourStats)
+
+router.route('/monthly-plan/:year').get(getMonthlyPlan)
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
