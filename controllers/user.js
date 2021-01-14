28ab7ca0b276a2d97f69809e3cc70fb0ea9060b8
gsonly <gsonly.imo@gmail.com>
2021-01-14 07:56:00 +0300

    feat: users

--- controllers/user.js
+++ controllers/user.js
@@ -1,27 +1,39 @@
-exports.getUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'not implemented',
+const { User } = require('../models')
+const { catchAsync } = require('../utils')
+
+exports.getUsers = catchAsync(async (req, res) => {
+  const users = await User.find({})
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   })
-}
+})
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'not implemented',
   })
 }
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'not implemented',
   })
 }
+
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'not implemented',
   })
 }
+
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
