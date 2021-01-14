27121d4e12daa7b412f6ba3b0314e1aff7631d88
gsonly <gsonly.imo@gmail.com>
2021-01-14 07:55:11 +0300

    fix: use correct status code

--- controllers/tour.js
+++ controllers/tour.js
@@ -42,7 +42,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
 exports.createTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.create(req.body)
 
-  res.status(200).json({
+  res.status(201).json({
     status: 'success',
     data: {
       tour,
