7c4b21a3d191134c72487cf2d50cb2e1c30ec672
gsonly <gsonly.imo@gmail.com>
2021-01-24 09:57:02 +0300

    chore: this is cool

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 const { tourRoutes, userRoutes } = require('./routes')
 const { IN_PROD } = require('./config')
 const { AppError } = require('./utils')
@@ -9,9 +14,31 @@ const { globalErrorHandler } = require('./controllers')
 const assetspath = path.join(__dirname, '/public')
 const app = express()
 
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 100,
+  message: 'too many requests from this IP, try again in an hour',
+})
+
+const hppOpts = hpp({
+  whitelist: [
+    'duration',
+    'ratingsQuantity',
+    'ratingsAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price',
+  ],
+})
+
 if (!IN_PROD) app.use(morgan('dev'))
 
-app.use(express.json())
+app.use(helmet())
+app.use('/api', limiter)
+app.use(express.json({ limit: '10kb' }))
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hppOpts)
 app.use(express.static(assetspath))
 app.use('/api/v1/tours', tourRoutes)
 app.use('/api/v1/users', userRoutes)
